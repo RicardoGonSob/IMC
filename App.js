0d0643a982a0ec0f892dc051c552ee98580a9b61
RicardoGonSob <21470005@campeche.tecnm.mx>
2024-10-14 09:53:11 -0600

    Modificacion de estilos

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Peso de la persona en kilogramos' style={styles.textInput}/>
+        <TextInput placeholder='Estatura de la persona en metros' style={styles.textInput}/>
+      </View>
+
+      <View style={styles.comandsContainer}>
+        <Button title='Calcular' color={'green'}/>
+        <Button title='Limpiar' color={'red'}/>
+      </View>
+
+      <View style={styles.resultsContainer}>
+        <Text style={styles.resultText}>El IMC de la persona es:</Text>
+        <Text style={styles.results}>24.36</Text>
+        <Text style={styles.resultText}>El estado nutricional de la persona es:</Text>
+        <Text style={styles.results}>Peso normal</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 40,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    // gap: 50,
+    alignItems: 'fill',
+    justifyContent: 'top',
   },
+  inputContainer: {
+    flex: 0,
+    borderColor: 'lightgray',
+    borderWidth: 2,
+    padding: 8,
+  },
+  comandsContainer: {
+    flex: 0,
+    // borderColor: 'lightgray',
+    // borderWidth: 3,
+    flexDirection: 'row',
+    marginVertical: 24,
+    justifyContent: 'center',
+    gap: 48,
+  },
+  resultsContainer: {
+    flex: 0,
+    borderColor: 'lightgray',
+    borderWidth: 2,
+    padding: 8,
+  },
+  textInput: {
+    borderColor: 'lightgray',
+    borderWidth: 2.5,
+    margin: 4,
+    fontSize: 18,
+  },
+  resultText: {
+    fontSize: 18,
+  },
+  results: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 16
+  }
 });
